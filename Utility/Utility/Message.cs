54e2dfd5e609dbb2fcdf1d840346c4b9c3e649bc
Swapnil Mirjolkar <swapnilm@maqsoftware.com>
2016-10-28 12:17:22 +0530

    Added initial messsage library

---
+++ Utility/Utility/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    class Message
+    {
+        public static void ShowErrorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void ShowSuccessMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+    }
+}
